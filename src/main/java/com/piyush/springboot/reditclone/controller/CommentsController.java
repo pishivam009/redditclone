1610ecd9b7c9b36fbda392b289cb0ecdbedfd2d1
pishivam009 <pishivam009@gmail.com>
2021-12-21 23:37:28 +0530

    Some progress on Comments controller

--- src/main/java/com/piyush/springboot/reditclone/controller/CommentsController.java
+++ src/main/java/com/piyush/springboot/reditclone/controller/CommentsController.java
@@ -1,10 +1,14 @@
 package com.piyush.springboot.reditclone.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.piyush.springboot.reditclone.dto.CommentsDto;
@@ -18,11 +22,22 @@ import lombok.AllArgsConstructor;
 public class CommentsController {
 
 	private final CommentService commentService;
-	
-	@PostMapping
+
+    @PostMapping
     public ResponseEntity<Void> createComment(@RequestBody CommentsDto commentsDto) {
         commentService.createComment(commentsDto);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(CREATED);
+    }
+	
+	@GetMapping
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForPost(@RequestParam("postId") Long postId) {
+        return status(OK)
+                .body(commentService.getCommentByPost(postId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CommentsDto>> getAllCommentsByUser(@RequestParam("userName") String userName) {
+        return status(OK).body(commentService.getCommentsByUser(userName));
     }
 	
 }
--- src/main/java/com/piyush/springboot/reditclone/service/CommentService.java
+++ src/main/java/com/piyush/springboot/reditclone/service/CommentService.java
@@ -1,12 +1,5 @@
 package com.piyush.springboot.reditclone.service;
 
-import com.piyush.springboot.reditclone.dto.CommentsDto;
-
 public class CommentService {
-
-	public void createComment(CommentsDto commentsDto) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
